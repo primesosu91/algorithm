d77b2fe3618748521a16c585a141861aea10c754
primesosu91 <ryosuke.sosu.91@gmail.com>
2026-09-07 23:18:24 +0900

    Add examples

--- data_structure/UnionFind.cpp
+++ data_structure/UnionFind.cpp
@@ -90,6 +90,91 @@ void yosupo_judge_UnionFind() {
     }
 }
 
+void ABC_049_D() {
+    int N, K, L;
+    cin >> N >> K >> L;
+    UnionFind uf_road(N), uf_train(N);
+    for (int i = 0; i < K; i ++) {
+        int p, q;
+        cin >> p >> q;
+        p --, q --;
+        uf_road.merge(p, q);
+    }
+    for (int i = 0; i < L; i ++) {
+        int r, s;
+        cin >> r >> s;
+        s --, r --;
+        uf_train.merge(r, s);
+    }
+    map<pair<int, int>, int> mp;
+    for (int i = 0; i < N; i ++) {
+        int r_road = uf_road.leader(i);
+        int r_train = uf_train.leader(i);
+        mp[{r_road, r_train}] ++;
+    }
+    for (int i = 0; i < N; i ++) {
+        int r_road = uf_road.leader(i);
+        int r_train = uf_train.leader(i);
+        cout << mp[{r_road, r_train}] << endl;
+        if (i < N - 1) cout << " ";
+        else cout << endl;
+    }
+}
+
+void ABC_380_E() {
+    int N, Q;
+    cin >> N >> Q;
+    UnionFind uf(N);
+    vector<int> col(N);
+    vector<int> num(N, 1);
+    vector<pair<int, int>> I(N);
+    for (int q = 0; q < N; q ++) {
+        I[q] = {q, q+1};
+    }
+    for (int i = 0; i < N; i ++) col[i] = i;
+    for (int q = 0; q < Q; q ++) {
+        int query;
+        cin >> query;
+        if (query == 1) {
+            int x, c;
+            cin >> x >> c;
+            x --, c --;
+            int root = uf.leader(x);
+            num[col[root]] -= uf.size(root);
+            col[root] = c;
+            num[c] += uf.size(root);
+            if (I[root].first > 0) {
+                int root_left = uf.leader(I[root].first-1);
+                if (col[root] == col[root_left]) {
+                    int left = I[root_left].first;
+                    int right = I[root].second;
+                    int nv = uf.merge(root, root_left);
+                    I[nv] = {left, right};
+                    col[nv] = col[root];
+                }
+            }
+            root = uf.leader(x);
+            if (I[root].second < N) {
+                int root_right = uf.leader(I[root].second);
+                if (col[root] == col[root_right]) {
+                    int left = I[root].first;
+                    int right = I[root_right].second;
+                    int nv = uf.merge(root, root_right);
+                    I[nv] = {left, right};
+                    col[nv] = col[root];
+                }
+            }
+        } else {
+            int c;
+            cin >> c;
+            c --;
+            cout << num[c] << endl;
+        }
+    }
+}
+
 int main() {
     yosupo_judge_UnionFind();
+    // ABC_049_D(); 道路・鉄道のどちらでも行ける都市の数
+    // ABC_380_E(); 左右の区間と同じ色なら区間をマージ
 }
